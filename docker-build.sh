ae0c4d09298bacb3ec6afb3edf4c4021533d4092
samsamsue <yiroon@qq.com>
2026-04-09 11:22:29 +0800

    update

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,13 @@
-docker rm -f v2nuxt \
-docker build -t v2nuxt . \
-docker tag v2nuxt yiroon/v2nuxt:latest \
-docker push yiroon/v2ex-reader:latest
+#!/bin/bash
+
+# 强制删除旧容器（如果有的话）
+docker rm -f v2nuxt || true
+
+# 构建镜像
+docker build -t v2nuxt .
+
+# 给镜像打标签
+docker tag v2nuxt yiroon/v2nuxt:latest
+
+# 推送到远程仓库
+docker push yiroon/v2nuxt:latest
